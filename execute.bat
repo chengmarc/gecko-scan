0a3f5f70a942fe069d99c5f540292600053a4729
Cheng <uzcheng@intranet.rexel.ca>
2023-09-12 12:06:39 -0400

    create virual environment and automatic dependencies installation

--- execute.bat
+++ execute.bat
@@ -1,3 +1,26 @@
+@echo off
+
+python -m venv __venv__
+call __venv__\Scripts\activate.bat
+pip install dependencies.tar.gz
+echo.
+echo Virtual environment setup completed.
+pause
+
+echo.
+echo ##########################
+echo ##### Execution No.1 #####
+echo ##########################
+echo.
 call gecko_scan_all_crypto.py
+
+echo.
+echo ##########################
+echo ##### Execution No.2 #####
+echo ##########################
+echo.
 call gecko_scan_categories.py
+
+echo.
+echo Execution fully completed.
 pause
