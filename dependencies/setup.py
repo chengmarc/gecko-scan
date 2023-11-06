6927c5c6d12dd82f1db749fb0a4488e463e068cf
Cheng <uzcheng@intranet.rexel.ca>
2023-11-05 21:53:15 -0500

    updated dependencies to version 1.2

--- dependencies/setup.py
+++ dependencies/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gsl-dependencies',
-    version='1.1',
+    version='1.2',
     packages=find_packages(),
-    install_requires=['tk==0.1.0', 'requests==2.31.0', 'pandas==2.1.0', 'bs4==0.0.1', 'colorama==0.4.6']
+    install_requires=['tk==0.1.0', 'requests==2.31.0', 'pandas==2.1.0', 'bs4==0.0.1']
 )
