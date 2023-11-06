45b11a345dc5c8523045dbba81b8ffdf79bcacff
Cheng <uzcheng@intranet.rexel.ca>
2023-11-05 21:54:25 -0500

    disabled console output in distribution

--- core-process/gecko_scan_distribute.bat
+++ core-process/gecko_scan_distribute.bat
@@ -1,2 +1,2 @@
-pyinstaller --distpath .\distribution --workpath .\distribution --onefile --add-data "gecko_scan_icon.ico;." --icon=gecko_scan_icon.ico gecko_scan_gui.py
+pyinstaller --distpath .\distribution --workpath .\distribution --onefile --noconsole --name GeckoScan --add-data "gecko_scan_icon.ico;." --icon=gecko_scan_icon.ico gecko_scan_gui.py
 pause
